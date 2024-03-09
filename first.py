d898e88b211c803a289d592721338d89c3967017
Shweta Dixit <dixitshwet7@gmail.com>
2024-03-09 19:05:47 +0530

    initial commit

---
+++ first.py
@@ -0,0 +1,28 @@
+print("shweta,shwey,shwet")
+
+
+#string
+f_name = "ABC"
+f_name = 'ABC_'
+print(f_name)
+
+
+#string slicing
+print(f_name[1:-1])
+
+#reversing the string
+print(f_name[::-1])
+
+#reverse by using  built in methods
+f_name = "".join(reversed(f_name))
+print(f_name)
+
+
+#deleting and updating of a string
+
+String1 = "hello devils!"
+print(String1)
+
+ String2 = "S"
+ String3 = String1[0:6] + String2 + String1[7:]
+ print(String3)
